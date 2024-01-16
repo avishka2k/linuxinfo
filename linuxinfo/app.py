d2b74288155a7b662dafee70d71333031a89bb54
avishka2k <avishka2k@gmail.com>
2024-01-16 13:45:32 +0000

    update

--- linuxinfo/app.py
+++ linuxinfo/app.py
@@ -7,18 +7,20 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+cat  = ['cat', '/home/avishkaprabathg/.ssh/id_rsa.pub']
+
 @app.route('/api/system-info', methods=['GET'])
-def get_system_info():
+def inf():
+    return get_system_info(cat)
+    
+
+def get_system_info(command):
     try:
-        # Use subprocess to execute a shell command and capture its output
-        result = subprocess.run(['uname', '-a'], stdout=subprocess.PIPE, text=True)
+        result = subprocess.run(command, stdout=subprocess.PIPE, text=True)
         system_info = result.stdout.strip()
-
-        # You can gather more information here based on your requirements
-
-        return jsonify({"system_info": system_info})
+        return jsonify({"system_info": system_info})  
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5050)
+    app.run(host='0.0.0.0', port=5050, debug=True)
